5d648bfac35c38767d6b3c8b508a7d3aae899d32
Ivan De Marino <detronizator@gmail.com>
2013-11-05 13:47:32 +0000

    Removing dependency on YUI.

---
+++ js/normalize.js
@@ -0,0 +1,287 @@
+var normalize = normalize || {};
+
+normalize.elementAddEventListener = function (element, eventName, callback, useCapture) {
+    useCapture = useCapture || false;
+
+    if (element.addEventListener) {
+        element.addEventListener(eventName, callback, useCapture);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + eventName, callback);
+    }
+};
+
+// ------------------------------------------------------------------- Polyfills
+// String.prototype.contains(string, startIndex)
+if(!("contains" in String.prototype)) {
+    String.prototype.contains = function(str, startIndex) {
+        return -1 !== String.prototype.indexOf.call(this, str, startIndex);
+    };
+}
+
+/*
+ * classList.js: Cross-browser full element.classList implementation.
+ * 2012-11-15
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public Domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ */
+
+/*global self, document, DOMException */
+
+/*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js*/
+
+if (typeof document !== "undefined" && !("classList" in document.documentElement)) {
+
+(function (view) {
+
+"use strict";
+
+if (!('HTMLElement' in view) && !('Element' in view)) return;
+
+var
+      classListProp = "classList"
+    , protoProp = "prototype"
+    , elemCtrProto = (view.HTMLElement || view.Element)[protoProp]
+    , objCtr = Object
+    , strTrim = String[protoProp].trim || function () {
+        return this.replace(/^\s+|\s+$/g, "");
+    }
+    , arrIndexOf = Array[protoProp].indexOf || function (item) {
+        var
+              i = 0
+            , len = this.length
+        ;
+        for (; i < len; i++) {
+            if (i in this && this[i] === item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    // Vendors: please allow content code to instantiate DOMExceptions
+    , DOMEx = function (type, message) {
+        this.name = type;
+        this.code = DOMException[type];
+        this.message = message;
+    }
+    , checkTokenAndGetIndex = function (classList, token) {
+        if (token === "") {
+            throw new DOMEx(
+                  "SYNTAX_ERR"
+                , "An invalid or illegal string was specified"
+            );
+        }
+        if (/\s/.test(token)) {
+            throw new DOMEx(
+                  "INVALID_CHARACTER_ERR"
+                , "String contains an invalid character"
+            );
+        }
+        return arrIndexOf.call(classList, token);
+    }
+    , ClassList = function (elem) {
+        var
+              trimmedClasses = strTrim.call(elem.className)
+            , classes = trimmedClasses ? trimmedClasses.split(/\s+/) : []
+            , i = 0
+            , len = classes.length
+        ;
+        for (; i < len; i++) {
+            this.push(classes[i]);
+        }
+        this._updateClassName = function () {
+            elem.className = this.toString();
+        };
+    }
+    , classListProto = ClassList[protoProp] = []
+    , classListGetter = function () {
+        return new ClassList(this);
+    }
+;
+// Most DOMException implementations don't allow calling DOMException's toString()
+// on non-DOMExceptions. Error's toString() is sufficient here.
+DOMEx[protoProp] = Error[protoProp];
+classListProto.item = function (i) {
+    return this[i] || null;
+};
+classListProto.contains = function (token) {
+    token += "";
+    return checkTokenAndGetIndex(this, token) !== -1;
+};
+classListProto.add = function () {
+    var
+          tokens = arguments
+        , i = 0
+        , l = tokens.length
+        , token
+        , updated = false
+    ;
+    do {
+        token = tokens[i] + "";
+        if (checkTokenAndGetIndex(this, token) === -1) {
+            this.push(token);
+            updated = true;
+        }
+    }
+    while (++i < l);
+
+    if (updated) {
+        this._updateClassName();
+    }
+};
+classListProto.remove = function () {
+    var
+          tokens = arguments
+        , i = 0
+        , l = tokens.length
+        , token
+        , updated = false
+    ;
+    do {
+        token = tokens[i] + "";
+        var index = checkTokenAndGetIndex(this, token);
+        if (index !== -1) {
+            this.splice(index, 1);
+            updated = true;
+        }
+    }
+    while (++i < l);
+
+    if (updated) {
+        this._updateClassName();
+    }
+};
+classListProto.toggle = function (token, forse) {
+    token += "";
+
+    var
+          result = this.contains(token)
+        , method = result ?
+            forse !== true && "remove"
+        :
+            forse !== false && "add"
+    ;
+
+    if (method) {
+        this[method](token);
+    }
+
+    return !result;
+};
+classListProto.toString = function () {
+    return this.join(" ");
+};
+
+if (objCtr.defineProperty) {
+    var classListPropDesc = {
+          get: classListGetter
+        , enumerable: true
+        , configurable: true
+    };
+    try {
+        objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+    } catch (ex) { // IE 8 doesn't support enumerable:true
+        if (ex.number === -0x7FF5EC54) {
+            classListPropDesc.enumerable = false;
+            objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+        }
+    }
+} else if (objCtr[protoProp].__defineGetter__) {
+    elemCtrProto.__defineGetter__(classListProp, classListGetter);
+}
+
+}(self));
+
+}
+
+// -------------------------------------- Douglas Crockford: Remedial Javascript
+function typeOf(value) {
+    var s = typeof value;
+    if (s === 'object') {
+        if (value) {
+            if (Object.prototype.toString.call(value) == '[object Array]') {
+                s = 'array';
+            }
+        } else {
+            s = 'null';
+        }
+    }
+    return s;
+}
+
+function isEmpty(o) {
+    var i, v;
+    if (typeOf(o) === 'object') {
+        for (i in o) {
+            v = o[i];
+            if (v !== undefined && typeOf(v) !== 'function') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+if (!String.prototype.entityify) {
+    String.prototype.entityify = function () {
+        return this.replace(/&/g, "&amp;").replace(/</g,
+            "&lt;").replace(/>/g, "&gt;");
+    };
+}
+
+if (!String.prototype.quote) {
+    String.prototype.quote = function () {
+        var c, i, l = this.length, o = '"';
+        for (i = 0; i < l; i += 1) {
+            c = this.charAt(i);
+            if (c >= ' ') {
+                if (c === '\\' || c === '"') {
+                    o += '\\';
+                }
+                o += c;
+            } else {
+                switch (c) {
+                case '\b':
+                    o += '\\b';
+                    break;
+                case '\f':
+                    o += '\\f';
+                    break;
+                case '\n':
+                    o += '\\n';
+                    break;
+                case '\r':
+                    o += '\\r';
+                    break;
+                case '\t':
+                    o += '\\t';
+                    break;
+                default:
+                    c = c.charCodeAt();
+                    o += '\\u00' + Math.floor(c / 16).toString(16) +
+                        (c % 16).toString(16);
+                }
+            }
+        }
+        return o + '"';
+    };
+}
+
+if (!String.prototype.supplant) {
+    String.prototype.supplant = function (o) {
+        return this.replace(
+            /\{([^{}]*)\}/g,
+            function (a, b) {
+                var r = o[b];
+                return typeof r === 'string' || typeof r === 'number' ? r : a;
+            }
+        );
+    };
+}
+
+if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+        return this.replace(/^\s*(\S*(?:\s+\S+)*)\s*$/, "$1");
+    };
+}
